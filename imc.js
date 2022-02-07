a939a7bfd31d95691dd38154eebe06ab71afbff9
dfpires@gmail.com <dfpires@gmail.com>
2022-02-07 12:26:28 -0300

    final da primeira aula

---
+++ imc.js
@@ -0,0 +1,10 @@
+function calculaImc(){
+    // recupera os dados digitados pelo usuário
+    // JS é não tipado => não se define o tipo da variável
+    let peso = document.getElementById("peso").value
+    let altura = document.getElementById("altura").value
+    // calcular o IMC
+    let imc = (peso / (altura * altura))
+    // mostra o resultado para o usuário
+    alert("O IMC é " + imc.toFixed(2)) // duas casas depois da vírgula
+}
